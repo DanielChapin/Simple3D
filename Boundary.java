d13612356774ea5088b1e733306a7ea8dd30c0a4
Daniel Chapin <dchapes123@gmail.com>
2020-11-19 11:46:47 -0600

    Added Boundary.random

--- Boundary.java
+++ Boundary.java
@@ -12,6 +12,13 @@ public class Boundary extends LineSegment {
         super(start, finish);
         this.color = color;
     }
+
+    public static Boundary random(float xMin, float xMax, float yMin, float yMax) {
+        Vector start = Vector.random(xMin, xMax, yMin, yMax);
+        Vector finish = Vector.random(xMin, xMax, yMin, yMax);
+        Color color = new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
+        return new Boundary(start, finish, color);
+    }
     
     public Color getColor() {
         return color;
