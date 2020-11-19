87e36ac989162181e962329e372e646e2b5c92f9
Daniel Chapin <dchapes123@gmail.com>
2020-11-18 22:03:51 -0600

    Cleaned up Game.java

--- Game.java
+++ Game.java
@@ -7,12 +7,10 @@ public class Game {
     JFrame frame;
     Canvas canvas;
 
+    RayCaster player;
+
     public Game(JFrame frame, Canvas canvas) {
-        LineSegment testLine = new LineSegment(new Vector(0, 10), new Vector(10, 10));
-        Ray testRay = new Ray(new Vector(5, 0), new Vector(0, 1));
-        Vector intersection = testRay.getIntersection(testLine);
-        System.out.println(intersection);
-        System.out.println(intersection.x);
+        player = new RayCaster(new Vector(0), 90, 10);
     }
 
     public void update(float deltaTime) {
